21d58c53f09456202b29bb24fb62708a21f5b8b9
WAHEEDIANHO <waheedianho65@gmail.com>
2024-05-15 12:14:16 +0100

    2024-05-15

--- switch.sh
+++ switch.sh
@@ -6,6 +6,6 @@ git commit -m "$CURRENT_DATE"
 git remote add origin https://github.com/WAHEEDIANHO/central-lms-home.git
 git push -u origin master
 git remote remove origin
-#git remote add origin https://github.com/SeyiKodek/Central-LMS.git
-#git push -u origin master
-#git remote remove origin
+git remote add origin https://github.com/SeyiKodek/Central-LMS.git
+git push -u origin master
+git remote remove origin
